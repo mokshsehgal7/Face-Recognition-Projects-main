8b85e55167fdf0cdb572d77335c0b49b08b09ba0
mokshsehgal7 <120220260@umail.ucc.ie>
2021-06-30 00:33:16 +0100

    Flask update

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ while True:
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.rectangle(img, (x1, y2 - 5), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
             # cv2.imshow('cam', img)
             # if cv2.waitKey(1)==13:
@@ -97,7 +97,7 @@ while True:
             #     break
 
     cv2.imshow('cam', img)
-    if cv2.waitKey(1)==13:
+    if cv2.waitKey(1) == 13:
         break
 cap.release()
 cv2.destroyAllWindows()
